8a802435be4e1bcbad3e4e655a3a626166562b86
Alan Giacomin <alan.giacomin@gmail.com>
2022-03-01 22:17:17 +0100

    linter

--- src/LazyElement.jsx
+++ src/LazyElement.jsx
@@ -3,11 +3,12 @@ import { lazy, Suspense } from 'react';
 const LazyElement = (func, fallback) => {
   setTimeout(func, 1000);
   const LoadedComponent = lazy(func);
-  return (props) => (
+  const suspenseComponent = (props) => (
     <Suspense fallback={fallback || null}>
       <LoadedComponent {...props} />
     </Suspense>
   );
+  return suspenseComponent;
 };
 
 export default LazyElement;
